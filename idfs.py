4403dbb9db6003f950a5476ff9ecef2af3f65479
Adrian Gaj <fcb970@interia.pl>
2019-10-29 13:20:23 +0100

    implement idfs finish

--- idfs.py
+++ idfs.py
@@ -1,21 +1,24 @@
-from node import Node
-from random import shuffle
-import table
 import utils
-
+import time
 import dfs
 
 
 def idfs(order, solved_table, begin_table, min_limit, max_depth):
+    time_before = time.time()
 
     # Iterative deepening DFS
     print("Inside IDFS algorithm")
 
-    final_node = dfs.search(begin_table, solved_table, order)
+    for limit in range(min_limit, max_depth):
+        final_node = dfs.search(begin_table, solved_table, order, limit)
+        if final_node is not None:
+            print("Solution found in " + str(time.time() - time_before) + 's')
 
-    if final_node is None:
-        print("SOLUTION NOT FOUND - REACHED MAX DEPTH")
-    else:
-        print("SOLUTION FOUND")
+            moves = utils.create_list_of_moves(final_node)
+            print("Moves to solve the puzzle: " + str(len(moves)))
+            print(utils.convert_moves(moves))
+            return
+
+    print("SOLUTION NOT FOUND - REACHED MAX DEPTH")
 
 
