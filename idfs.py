cdf6a5d675ce8ac0f07b2bb0de2515d655d54137
Adrian Gaj <fcb970@interia.pl>
2019-10-29 13:01:30 +0100

    Small changes

--- idfs.py
+++ idfs.py
@@ -1,2 +1,21 @@
-def idfs(order, table):
+from node import Node
+from random import shuffle
+import table
+import utils
+
+import dfs
+
+
+def idfs(order, solved_table, begin_table, min_limit, max_depth):
+
+    # Iterative deepening DFS
     print("Inside IDFS algorithm")
+
+    final_node = dfs.search(begin_table, solved_table, order)
+
+    if final_node is None:
+        print("SOLUTION NOT FOUND - REACHED MAX DEPTH")
+    else:
+        print("SOLUTION FOUND")
+
+
