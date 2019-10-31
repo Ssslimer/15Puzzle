bd7cffd78e1da0d667e2cbd760ff48eea561fa12
Ssslimer <maciek.olejnik@tlen.pl>
2019-10-31 20:22:37 +0100

    Optimized Table

--- table.py
+++ table.py
@@ -2,17 +2,22 @@ from utils import ORDER_UP, ORDER_DOWN, ORDER_RIGHT, ORDER_LEFT
 
 
 class Table(object):
-    def __init__(self, data):
+    def __init__(self, data, blank_row=-1, blank_column=-1):
         self.data = tuple(tuple(item) for item in data)
         self.columns = len(data)
         self.rows = len(data[0])
 
-        self.blank_row, self.blank_column = self.find_blank_tile_pos()
+        if blank_row == -1 or blank_column == -1:
+            self.blank_row, self.blank_column = self.find_blank_tile_pos()
+        else:
+            self.blank_row = blank_row
+            self.blank_column = blank_column
+
         self.hash_value = hash(self.data)
 
     def find_blank_tile_pos(self):
-        for row in range(len(self.data)):
-            for column in range(len(self.data[row])):
+        for row in range(self.rows):
+            for column in range(self.columns):
                 if self.data[row][column] == 0:
                     return row, column
 
@@ -21,9 +26,9 @@ class Table(object):
     def count_wrong_puzzles(self, proper_table):
         counter = 0
 
-        for row in range(len(self.data)):
-            for column in range(1, len(self.data[row])+1):
-                if self.data[row][column-1] != proper_table.data[row][column-1]:
+        for row in range(self.rows):
+            for column in range(self.columns):
+                if self.data[row][column] != proper_table.data[row][column]:
                     counter += 1
 
         return counter
@@ -55,15 +60,15 @@ class Table(object):
         if direction == ORDER_LEFT:
             return self.blank_column >= 1
         elif direction == ORDER_RIGHT:
-            return self.blank_column < len(self.data[0])-1
+            return self.blank_column < self.columns-1
         elif direction == ORDER_UP:
             return self.blank_row >= 1
         elif direction == ORDER_DOWN:
-            return self.blank_row < len(self.data)-1
+            return self.blank_row < self.rows-1
 
     def find_value(self, value):
-        for row in range(len(self.data)):
-            for column in range(len(self.data[row])):
+        for row in range(self.rows):
+            for column in range(self.columns):
                 if self.data[row][column] == value:
                     return row, column
 
@@ -77,4 +82,4 @@ class Table(object):
 
         new_data[self.blank_row][self.blank_column] = new_data[new_blank_row][new_blank_column]
         new_data[new_blank_row][new_blank_column] = 0
-        return Table(new_data)
+        return Table(new_data, new_blank_row, new_blank_column)
